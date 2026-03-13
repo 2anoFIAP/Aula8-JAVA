564d22c23d09dc6a0d577a164440cad4f47b68e2
Guilherme de Araujo Moreira <guiam2909@gmail.com>
2026-03-13 09:12:43 -0300

    Refazendo JOp com as normas pedidas no execicio

--- src/br/com/fiap/main/MainJOptionPane.java
+++ src/br/com/fiap/main/MainJOptionPane.java
@@ -1,18 +1,41 @@
 package br.com.fiap.main;
 
-import javax.swing.JOptionPane;
-import br.com.fiap.bean.FolhaPag;
+import br.com.fiap.bean.FolhaDePagamento;
 
-public class MainJOptionPane {
+import javax.swing.*;
+import java.util.Scanner;
+
+public class MainJOpttionPane {
     public static void main(String[] args) {
-        FolhaPag fp = new FolhaPag();
+        FolhaDePagamento folha = new FolhaDePagamento();
+        double slb, INSS, vps;
+        int nd;
+        String auxiliar;
+        try {
+            //Leitura da informações
+            JOptionPane.showMessageDialog(null,"Sistema para calcular seu salário liquido");
+            auxiliar = JOptionPane.showInputDialog("Digite o seu salário bruto: ");
+            slb = Double.parseDouble(auxiliar);
+            auxiliar = JOptionPane.showInputDialog("Digite o desconto do INSS: ");
+            INSS = Double.parseDouble(auxiliar);
+            auxiliar = JOptionPane.showInputDialog("Insirá o valor do plano de saúde: ");
+            vps = Double.parseDouble(auxiliar);
+            auxiliar = JOptionPane.showInputDialog("Informe o seu número de dependentes: ");
+            nd = Integer.parseInt(auxiliar);
 
-        fp.salarioBruto = Double.parseDouble(JOptionPane.showInputDialog("Digite o Bruto: "));
-        fp.numeroDeDependentes = Integer.parseInt(JOptionPane.showInputDialog("Dependentes: "));
+            //Armazenando valores das variáveis nos respectivos atributos
+            folha.salarioBruto = slb;
+            folha.desontoINSS = INSS;
+            folha.valorPlanoDeSaude = vps;
+            folha.numeroDeDependentes = nd;
 
-        double liquido = fp.calcularSalarioLiquido();
-
-        String resultado = String.format("O salario liquido vai ser: R$ %.2f", liquido);
-        JOptionPane.showMessageDialog(null, resultado);
+            //Exibindo informações e resultado
+            String msg = String.format("Salário Bruto: R$ %.2f\nDependentes: %d\nINSS: %.1f\nPlano de saúde: %.2f\nSalário liquido: R$ %.2f", folha.salarioBruto, folha.numeroDeDependentes, folha.desontoINSS, folha.valorPlanoDeSaude, folha.calcularSalarioLiquido());
+            JOptionPane.showMessageDialog(null, msg);
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Informações inválidas");
+        }
     }
 }
+ 
