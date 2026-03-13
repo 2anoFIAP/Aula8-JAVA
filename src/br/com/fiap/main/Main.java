ccb4857e4e52fb08b353391f8afaab72b2473172
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-13 08:45:20 -0300

    Guardando informacoes na main para futuros testes, e optimizando o MainScanner

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -1,4 +1,25 @@
-package br.com.fiap.main;
+//package br.com.fiap.main;
 
-public class Main {
-}
+//import java.util.Scanner;
+//import br.com.fiap.bean.FolhaPag;
+
+//public class MainScanner {
+  //  public static void main(String[] args) {
+    //    Scanner leitor = new Scanner(System.in);
+      //  FolhaPag fp = new FolhaPag();
+
+        //System.out.print("Digite o salario bruto: ");
+//        fp.salarioBruto = leitor.nextDouble();
+
+  //      System.out.print("Numero de dependentes: ");
+    //    fp.numeroDeDependentes = leitor.nextInt();
+
+      //  double liquido = fp.calcularSalarioLiquido();
+
+//        System.out.printf("Salario Liquido: R$ %.2f %n", liquido);
+
+  //      leitor.close();
+   // }
+//}
+
+// Depois testar o codigomain e main scanner para saber qual e melhor
--- src/br/com/fiap/main/MainScanner.java
+++ src/br/com/fiap/main/MainScanner.java
@@ -5,18 +5,42 @@ import br.com.fiap.bean.FolhaPag;
 
 public class MainScanner {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
+        Scanner leitor;
         FolhaPag fp = new FolhaPag();
+        double salarioBruto, descontoINSS, valorPlanoDeSaude;
+        int numeroDeDependentes;
+        try{
+            leitor = new Scanner(System.in)
 
-        System.out.print("Digite o salario bruto: ");
-        fp.salarioBruto = leitor.nextDouble();
+            System.out.print("Digite o salario bruto: ");
+            fp.salarioBruto = leitor.nextDouble();
 
-        System.out.print("Numero de dependentes: ");
-        fp.numeroDeDependentes = leitor.nextInt();
+            System.out.print("Numero de dependentes: ");
+            fp.numeroDeDependentes = leitor.nextInt();
 
-        double liquido = fp.calcularSalarioLiquido();
+            System.out.println("Digite desconto INSS");
+            fp.descontoINSS = leitor.nextDouble();
 
-        System.out.printf("Salario Liquido: R$ %.2f %n", liquido);
+            Syste.out.println("Digte o valor do plano de saude: ")
+            fp.valorPlanoDeSaude = leitor.nextDouble();
+
+            FolhaPag.salarioBruto = salarioBruto;
+            FolhaPag.numeroDeDependentes = numeroDeDependentes;
+            FolhaPag.descontoINSS = descontoINSS;
+            FolhaPag.valorPlanoDeSaude = valorPlanoDeSaude;
+
+            double liquido = fp.calcularSalarioLiquido();
+            System.out.printf("Salario Liquido: R$ %.2f %n", liquido);
+
+        } catch (Exception Exception) {
+            System.out.println("Formato incorreto!")
+        }
+
+        
+
+        
+
+        
 
         leitor.close();
     }
